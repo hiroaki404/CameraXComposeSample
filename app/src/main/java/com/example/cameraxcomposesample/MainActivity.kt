bc68eddffde1f85563e3268ab84d066d47ecd89b
hiroaki404 <nosho404@gmail.com>
2025-02-10 19:23:50 +0900

    request permission and add manifest

--- app/src/main/java/com/example/cameraxcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/cameraxcomposesample/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.cameraxcomposesample.ui.theme.CameraXComposeSampleTheme
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.rememberPermissionState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +23,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             CameraXComposeSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CameraPermissionRequestEffect()
+
                     Greeting(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier.padding(innerPadding),
                     )
                 }
             }
@@ -30,11 +35,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun CameraPermissionRequestEffect() {
+    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+
+    LaunchedEffect(Unit) {
+        cameraPermissionState.launchPermissionRequest()
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
