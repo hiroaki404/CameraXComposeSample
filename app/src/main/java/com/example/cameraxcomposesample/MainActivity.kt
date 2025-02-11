2e08e2584da5c935605a536646ffc0f1acce887e
hiroaki404 <nosho404@gmail.com>
2025-02-11 09:03:47 +0900

    add preview

--- app/src/main/java/com/example/cameraxcomposesample/MainActivity.kt
+++ app/src/main/java/com/example/cameraxcomposesample/MainActivity.kt
@@ -4,62 +4,74 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.camera.compose.CameraXViewfinder
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.cameraxcomposesample.ui.PreviewViewModel
 import com.example.cameraxcomposesample.ui.theme.CameraXComposeSampleTheme
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CameraXComposeSampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CameraPermissionRequestEffect()
+                    val previewViewModel = PreviewViewModel()
+                    val cameraPermissionState =
+                        rememberPermissionState(android.Manifest.permission.CAMERA)
 
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding),
-                    )
+                    LaunchedEffect(Unit) {
+                        cameraPermissionState.launchPermissionRequest()
+                    }
+
+                    if (cameraPermissionState.status.isGranted) {
+                        PreviewView(
+                            modifier = Modifier.padding(innerPadding),
+                            viewModel = previewViewModel,
+                        )
+                    }
                 }
             }
         }
     }
 }
 
-@OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CameraPermissionRequestEffect() {
-    val cameraPermissionState = rememberPermissionState(android.Manifest.permission.CAMERA)
+fun PreviewView(modifier: Modifier = Modifier, viewModel: PreviewViewModel) {
+    val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
 
-    LaunchedEffect(Unit) {
-        cameraPermissionState.launchPermissionRequest()
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.bindToCamera(context.applicationContext, lifecycleOwner)
     }
-}
 
-@Composable
-fun Greeting(
-    name: String,
-    modifier: Modifier = Modifier,
-) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier,
-    )
+    surfaceRequest?.let {
+        CameraXViewfinder(
+            modifier = modifier.fillMaxSize(),
+            surfaceRequest = it,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CameraXComposeSampleTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/cameraxcomposesample/ui/PreviewViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.cameraxcomposesample.ui
+
+import android.content.Context
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceRequest
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.lifecycle.awaitInstance
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class PreviewViewModel : ViewModel() {
+    private var _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+    val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest.asStateFlow()
+
+    private val cameraPreviewUseCase = Preview.Builder().build().apply {
+        setSurfaceProvider { newSurfaceRequest ->
+            _surfaceRequest.update { newSurfaceRequest }
+        }
+    }
+
+    suspend fun bindToCamera(appContext: Context, lifecycleOwner: LifecycleOwner) {
+        val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
+        processCameraProvider.bindToLifecycle(
+            lifecycleOwner,
+            CameraSelector.DEFAULT_BACK_CAMERA,
+            cameraPreviewUseCase,
+        )
+        try {
+            awaitCancellation()
+        } finally {
+            processCameraProvider.unbindAll()
+        }
+    }
+}
